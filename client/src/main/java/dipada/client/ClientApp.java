1e8d25ecc2bf5063a2576d52b43bcae8a4b396e1
dan <daniele.dipalma68@edu.unito.it>
2022-05-06 11:38:05 +0200

    added login view and controller

--- client/src/main/java/dipada/client/ClientApp.java
+++ client/src/main/java/dipada/client/ClientApp.java
@@ -11,11 +11,16 @@ import java.net.URL;
 public class ClientApp extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        System.out.println("go");
-        URL clientUrl = ClientApp.class.getResource("MainWindow.fxml");
-        System.out.println("clienturl : " + clientUrl);
+
+        //URL clientUrl = ClientApp.class.getResource("MainWindow.fxml");
+        //FXMLLoader fxmlLoader = new FXMLLoader(clientUrl);
+        //Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+
+        URL clientUrl = ClientApp.class.getResource("Login.fxml");
+
         FXMLLoader fxmlLoader = new FXMLLoader(clientUrl);
-        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        System.out.println("qua");
+        Scene scene = new Scene(fxmlLoader.load(), 300, 100); // TODO spec dimensioni
 
         stage.setTitle("dipadamail");
         stage.setScene(scene);
---
+++ client/src/main/java/dipada/client/controller/LoginController.java
@@ -0,0 +1,13 @@
+package dipada.client.controller;
+
+import javafx.event.ActionEvent;
+
+public class LoginController {
+    public void initialize(){
+        System.out.println("hello login controller");
+    }
+
+    public void onLoginButtonClick(ActionEvent actionEvent) {
+        // TODO effettuare login
+    }
+}
