9d8d2857deaa015d2467a91a1f24374fc0209806
dan <daniele.dipalma68@edu.unito.it>
2022-05-06 11:54:55 +0200

    update start method for load 2 view, to fix: main layout just need appear later login layout is closed

--- client/src/main/java/dipada/client/ClientApp.java
+++ client/src/main/java/dipada/client/ClientApp.java
@@ -1,14 +1,18 @@
 package dipada.client;
 
+import dipada.client.controller.LoginController;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 
 public class ClientApp extends Application {
+
     @Override
     public void start(Stage stage) throws Exception {
 
@@ -18,16 +22,53 @@ public class ClientApp extends Application {
 
         URL clientUrl = ClientApp.class.getResource("Login.fxml");
 
-        FXMLLoader fxmlLoader = new FXMLLoader(clientUrl);
-        System.out.println("qua");
-        Scene scene = new Scene(fxmlLoader.load(), 300, 100); // TODO spec dimensioni
+        //FXMLLoader fxmlLoader = new FXMLLoader(clientUrl);
+        //System.out.println("qua");
+        //Scene scene = new Scene(fxmlLoader.load(), 300, 100); // TODO spec dimensioni
 
-        stage.setTitle("dipadamail");
-        stage.setScene(scene);
-        stage.show();
+        //stage.setTitle("dipadamail");
+        //stage.setScene(scene);
+        //stage.show();
+        showLoginLayout(stage);
+        showMainWindow(stage);
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    private void showLoginLayout(Stage primaryStage) {
+        URL loginUrl = ClientApp.class.getResource("Login.fxml");
+        FXMLLoader loginLoader = new FXMLLoader(loginUrl);
+        try {
+            Scene scene = new Scene(loginLoader.load(), 300, 100);
+            Stage stage = new Stage();
+            stage.setTitle("Login");
+
+            stage.initOwner(primaryStage);
+            stage.setScene(scene);
+            LoginController loginController = loginLoader.getController();
+            loginController.setLoginController(this, stage);
+            stage.show();
+        } catch (IOException e) {
+            System.out.println("Login loader error");
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private void showMainWindow(Stage primaryStage) {
+        try{
+            URL mainWindowsUrl = ClientApp.class.getResource("MainWindow.fxml");
+            FXMLLoader mainWindowLoader = new FXMLLoader(mainWindowsUrl);
+            Scene scene = new Scene(mainWindowLoader.load(), 900, 600);
+            primaryStage.setTitle("dipadamail");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            System.out.println("Main windows loader error");
+            e.printStackTrace();
+        }
     }
 }
--- client/src/main/java/dipada/client/controller/LoginController.java
+++ client/src/main/java/dipada/client/controller/LoginController.java
@@ -1,13 +1,23 @@
 package dipada.client.controller;
 
+import dipada.client.ClientApp;
 import javafx.event.ActionEvent;
+import javafx.stage.Stage;
 
 public class LoginController {
+    private ClientApp clientApp;
+    private Stage stage;
+
     public void initialize(){
         System.out.println("hello login controller");
     }
 
     public void onLoginButtonClick(ActionEvent actionEvent) {
         // TODO effettuare login
+    }
+
+    public void setLoginController(ClientApp clientApp, Stage stage) {
+        this.clientApp = clientApp;
+        this.stage = stage;
     }
 }
