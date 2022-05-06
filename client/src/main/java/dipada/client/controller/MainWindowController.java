c1982a08ec1b056b53329a0f0d0a04ed4cb64fcf
dan <daniele.dipalma68@edu.unito.it>
2022-05-06 11:12:23 +0200

    added main window controller, image and update main view

---
+++ client/src/main/java/dipada/client/controller/MainWindowController.java
@@ -0,0 +1,10 @@
+package dipada.client.controller;
+
+import javafx.scene.input.MouseEvent;
+
+public class MainWindowController {
+    public void onDeleteButtonClick(MouseEvent mouseEvent) {
+        // TODO cancellare email
+    }
+
+}
