633c62c27011cb566f1652f8e494e5317f6736bc
dan <daniele.dipalma68@edu.unito.it>
2022-05-08 07:46:54 +0200

    added timestamp

--- server/src/main/java/prog/dipada/model/Log.java
+++ server/src/main/java/prog/dipada/model/Log.java
@@ -1,11 +1,17 @@
 package prog.dipada.model;
 
+import javafx.application.Platform;
 import javafx.beans.property.ListProperty;
 import javafx.beans.property.SimpleListProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.input.DataFormat;
 
 import java.security.PublicKey;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.LinkedList;
 
 /**
@@ -35,6 +41,7 @@ public class Log {
      *
      * */
     public void printLogOnScreen(String messageLog){
-        logContent.add(messageLog);
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Platform.runLater(()->logContent.add(dateFormatter.format(new Date()) + " - " + messageLog));
     }
 }
