5e4d5d0b737854f6e7ee03270ff158c06f7aede1
dan <daniele.dipalma68@edu.unito.it>
2022-05-13 15:54:37 +0200

    added clearComma by receiver string, checking for entire form and alert to display. To improve readability of alert msg

--- client/src/main/java/prog/dipada/controller/SendWindowController.java
+++ client/src/main/java/prog/dipada/controller/SendWindowController.java
@@ -2,15 +2,16 @@ package prog.dipada.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import prog.dipada.ClientApp;
 import prog.dipada.model.Email;
 
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SendWindowController {
     @FXML
@@ -44,7 +45,6 @@ public class SendWindowController {
         //rcvTextField.setText(this.email.getReceivers().toString());
         subjectTextField.setText(this.email.getSubject());
         msgTxtArea.setText(this.email.getMessageText());
-        System.out.println("Finito set email");
     }
 
     private String formatReceiversComma(List<String> receiversEmail) {
@@ -63,11 +63,73 @@ public class SendWindowController {
     }
 
     public void onClickSendBtn(ActionEvent actionEvent) {
-        System.out.println("Bottone cliccato");
         // TODO controllare indirizzi email
-        sendClicked = true;
-        stage.close();
 
+        email.setReceivers(clearCommaReceiver(rcvTextField.getText()));
+        email.setSubject(subjectTextField.getText());
+        email.setMessageText(msgTxtArea.getText());
+
+        if (checkSendEmailFields(email)) {
+            sendClicked = true;
+            // TODO inviare email
+            stage.close();
+        }
+    }
+
+    private boolean checkSendEmailFields(Email email) {
+        String errorContentMsg = "";
+        if (rcvTextField.getText() == null || rcvTextField.getText().length() == 0) {
+            errorContentMsg += "No receiver written\n";
+        }
+
+        for (String rcv : email.getReceivers()) {
+            if (!validateEmail(rcv)) {
+                errorContentMsg += "Receiver: " + rcv + " is not valid\n";
+            }
+        }
+
+        if (subjectTextField.getText() == null || subjectTextField.getText().length() == 0) {
+            errorContentMsg += "No Subject written\n";
+        }
+
+        if (msgTxtArea.getText() == null || msgTxtArea.getText().length() == 0) {
+            errorContentMsg += "Message body is empty\n";
+        }
+
+        if (errorContentMsg.length() == 0) {
+            return true;
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.initOwner(stage);
+            alert.setTitle("Error in fields");
+            alert.setHeaderText("There are some wrong things: ");
+            alert.setContentText(errorContentMsg);
+            alert.showAndWait();
+            return false;
+        }
+    }
+
+    private boolean validateEmail(String emailUserLogin) {
+        // Compiles the given regular expression and attempts to match the given input against it.
+        return Pattern.matches("^[a-zA-Z][a-zA-Z0-9._][a-zA-Z0-9._][a-zA-Z0-9._]+@dipada.it", emailUserLogin);
+    }
+
+    private List<String> clearCommaReceiver(String text) {
+        List<String> receivers = new LinkedList<>();
+
+        //Matcher matcher = Pattern.compile("[a-zA-Z][a-zA-Z0-9._][a-zA-Z0-9._][a-zA-Z0-9._]+").matcher(text);
+        Matcher matcher = Pattern.compile("[^,\s]+").matcher(text);
+        //dan,att ,ssta, dadas
+        while (matcher.find()) {
+            String group = matcher.group();
+            System.out.println("Trovato: " + group);
+            receivers.add(group);
+        }
+
+        System.out.println("Lunghezza lista " + receivers.size() + " Stampo: ");
+        for (String s : receivers)
+            System.out.println("\n" + s);
+        return receivers;
     }
 
     public boolean isSendClicked() {
