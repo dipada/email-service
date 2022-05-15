7acaff863030fa74ed770d718397963c066f1f3b
dan <daniele.dipalma68@edu.unito.it>
2022-05-15 13:26:40 +0200

    update login style, added main window style

--- client/src/main/java/prog/dipada/controller/MainWindowController.java
+++ client/src/main/java/prog/dipada/controller/MainWindowController.java
@@ -1,16 +1,12 @@
 package prog.dipada.controller;
 
 import javafx.application.Platform;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Popup;
 import javafx.stage.Stage;
@@ -23,6 +19,15 @@ import java.util.List;
 
 public class MainWindowController {
 
+
+    @FXML
+    private HBox bottomHbox;
+    @FXML
+    private VBox bottomVbox;
+    @FXML
+    private VBox topVbox;
+    @FXML
+    private HBox tophbox;
     @FXML
     private Label lblTotInbox;
     @FXML
